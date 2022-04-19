13167c310f4876f9462228aee129d3fb92eb00ea
John Hession <johhess@cdw.com>
2022-04-19 12:14:29 -0700

    Updating address checking

--- get_networking/generate_mock_networking.go
+++ get_networking/generate_mock_networking.go
@@ -1,49 +1,93 @@
 package get_networking
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 type SwitchData struct {
-	Size  string
-	Space string
-	Cidr  string
+	Size   string
+	Space  string
+	Cidr   string
+	Region string
 }
 
-func MakeTheSwitch(s SwitchData) {
+func MakeTheSwitch(s SwitchData) ([]string, error) {
 	switch s.Size {
 	case "grande":
 		if len(s.Cidr) != 0 {
-			BuildGrandeNetworks(s)
+			networks, err := BuildGrandeNetworks(s)
+			if err != nil {
+				return networks, err
+			}
+			return networks, nil
 		} else {
 			s.Cidr = "/24"
-			BuildGrandeNetworks(s)
+			networks, err := BuildGrandeNetworks(s)
+			if err != nil {
+				return networks, err
+			}
+			return networks, nil
 		}
 
 	case "venti":
 		if len(s.Cidr) != 0 {
-			BuildGrandeNetworks(s)
+			networks, err := BuildGrandeNetworks(s)
+			if err != nil {
+				return networks, err
+			}
+			return networks, nil
 		} else {
 			s.Cidr = "/23"
-			BuildGrandeNetworks(s)
+			networks, err := BuildGrandeNetworks(s)
+			if err != nil {
+				return networks, err
+			}
+			return networks, nil
 		}
 	default:
 		if len(s.Cidr) != 0 {
-			BuildGrandeNetworks(s)
+			networks, err := BuildGrandeNetworks(s)
+			if err != nil {
+				return networks, err
+			}
+			return networks, nil
 		} else {
 			s.Cidr = "/24"
-			BuildGrandeNetworks(s)
+			networks, err := BuildGrandeNetworks(s)
+			if err != nil {
+				return networks, err
+			}
+			return networks, nil
 		}
 	}
 }
 
 //BuildGrandeNetworks builds a 16 oz virtual network
-func BuildGrandeNetworks(a SwitchData) []string {
+func BuildGrandeNetworks(a SwitchData) ([]string, error) {
 	var s []string
-	next := fmt.Sprintf("%s%s", a.Space, a.Cidr)
-	return s
+	for i := 0; i < 128; i++ {
+		next := strings.Replace(strings.Split(a.Space, ".")[2], strings.Split(a.Space, ".")[2], strconv.Itoa(i+2), 2)
+		s = append(s, next)
+	}
+	if len(s) == 0 {
+		return s, fmt.Errorf("not able to build any grande address spaces in BuildGrandeNetworks")
+	} else {
+		return s, nil
+	}
 }
 
 //BuildVentiNetworks builds a 20oz virtual network
-func BuildVentiNetworks(a SwitchData) []string {
+func BuildVentiNetworks(a SwitchData) ([]string, error) {
 	var s []string
-	return s
+	for i := 0; i < 128; i++ {
+		next := strings.Replace(strings.Split(a.Space, ".")[2], strings.Split(a.Space, ".")[2], strconv.Itoa(i+1), 2)
+		s = append(s, next)
+	}
+	if len(s) == 0 {
+		return s, fmt.Errorf("not able to build any venti address spaces in BuildVentiNetworks")
+	} else {
+		return s, nil
+	}
 }
