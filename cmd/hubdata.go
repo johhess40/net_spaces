ff84a03236a65e7d1f2eb92de180d1e5142f4ab9
John Hession <johhess@cdw.com>
2022-04-24 14:44:13 -0700

    Adding in data returns for future use with hcl

--- cmd/hubdata.go
+++ cmd/hubdata.go
@@ -5,6 +5,7 @@ package cmd
 
 import (
 	"fmt"
+	net "github.com/johhess40/net_spaces/get_networking"
 	"log"
 
 	"github.com/spf13/cobra"
@@ -12,6 +13,7 @@ import (
 
 type HubBuilder struct {
 	Id      string
+	Type    string
 	OutType string
 }
 
@@ -25,18 +27,46 @@ var hubdataCmd = &cobra.Command{
 	Short: "hub-data returns data bout the hub that we want to connect to",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("hub-data called")
+		token, err := net.ExecToken()
+		if err != nil {
+			return
+		}
+		switch Hub.Type {
+		case "vhub":
+			data, err := net.GetVirtualHubData(Hub.Id, token)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(data)
+		case "vnet":
+			data, err := net.GetVirtualNetworkHubData(Hub.Id, token)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(data)
+		default:
+			data, err := net.GetVirtualHubData(Hub.Id, token)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(data)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(hubdataCmd)
-	hubdataCmd.Flags().StringVarP(&Hub.Id, "hub-id", "h", "", "the virtual hub id to connect the vnet to")
+	hubdataCmd.Flags().StringVarP(&Hub.Id, "hub-id", "i", "null", "the virtual hub id to connect the vnet to")
 	err := hubdataCmd.MarkFlagRequired("hub-id")
 	if err != nil {
 		log.Fatal(err)
 	}
-	hubdataCmd.Flags().StringVarP(&Hub.OutType, "out-type", "o", "", "output type for hub data being returned(hcl,json,yml)")
+	hubdataCmd.Flags().StringVarP(&Hub.Type, "hub-type", "t", "vhub", "hub type for lookup")
+	err = hubdataCmd.MarkFlagRequired("hub-type")
+	if err != nil {
+		log.Fatal(err)
+	}
+	hubdataCmd.Flags().StringVarP(&Hub.OutType, "out-type", "o", "json", "output type for hub data being returned(hcl,json,yml)")
 	err = hubdataCmd.MarkFlagRequired("out-type")
 	if err != nil {
 		log.Fatal(err)
