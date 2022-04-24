4725cc41a6d33bfc5949aec0e5aab48bb7a5f3ab
John Hession <johhess@cdw.com>
2022-04-24 14:50:23 -0700

    Adding in data returns for future use with hcl

---
+++ parsing/hcl.go
@@ -0,0 +1,10 @@
+package parsing
+
+type Vnet struct{}
+type Vhub struct{}
+type Connection struct{}
+
+func CreateHclObject() {}
+func BuildHclObject()  {}
+func ReturnHclObject() {}
+func WriteHclObject()  {}
